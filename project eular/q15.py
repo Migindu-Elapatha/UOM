bcfdbb1661baf97012c2a1d63ea436ef943326b6
Migindu-Elapatha <miginduelapatha@gmail.com>
2025-10-07 14:43:49 +0530

    q 15, q 19

---
+++ project eular/q15.py
@@ -0,0 +1,6 @@
+import math
+
+N = 20
+total_routes = math.comb(2 * N, N)
+
+print(total_routes)
---
+++ project eular/q19.py
@@ -0,0 +1,2 @@
+from datetime import date
+print(sum(date(y, m, 1).weekday() == 6 for y in range(1901, 2001) for m in range(1, 13)))
